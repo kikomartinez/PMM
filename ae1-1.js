30ca1f60645fe8ab87690bbf78dad81a9b26e8d4
quicomartinez11 <elok11@gmail.com>
2020-10-05 11:46:28 +0200

    AE1-1

---
+++ ae1-1.js
@@ -0,0 +1,4 @@
+console.log("Hola mundo");
+let alertMessage = ("Soy el primer script");
+const ALERT_MESSAGE = alertMessage + "y estoy funcionando sobre " + navigator.appVersion;
+alert(ALERT_MESSAGE);
