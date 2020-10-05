de70a390f939b71cde878b9d96212e46887bf39f
quicomartinez11 <elok11@gmail.com>
2020-10-05 11:52:16 +0200

    AE1-2

---
+++ ae1-2.js
@@ -0,0 +1,7 @@
+let months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.forEach(month => {
+    console.log(month)
+});
+
+console.table(months);
